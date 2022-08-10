f7a285ed3bc2425c745bee295bdc18590d20b801
ValtersSteinblums <109601588+ValtersSteinblums@users.noreply.github.com>
2022-08-10 20:11:47 +0300

    Initial Commit

---
+++ TempConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TempConverter
+//
+//  Created by valters.steinblums on 10/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
